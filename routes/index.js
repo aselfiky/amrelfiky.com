98657e3a50e0cbf55a6ac8affae584ca9dc042dc
Amr Elfiky <aelfiky@ucsd.edu>
2019-02-02 16:34:03 -0800

    auto render build, link css

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Amr Elfiky' });
 });
 
-module.exports = router;
+module.exports = app;
