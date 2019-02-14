a5cc386cb28b19fa381e4401fe9581f8022611c7
Amr Elfiky <aelfiky@ucsd.edu>
2019-02-13 19:41:19 -0800

    remove practice code

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,9 @@ router.get('/', function(req, res, next) {
   res.send('Coming soon...');
 });
 
+router.post('/myaction', function(req, res) {
+  res.send('You sent the name "' + req.body.name + '".');
+});
+
 module.exports = router;
 
